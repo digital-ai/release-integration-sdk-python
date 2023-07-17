a3ec7188f1c192345ca5c952fb7f6bd18877cbe8
Marin Milina <marin.milina@ag04.com>
2023-07-17 16:39:50 +0200

    S-92335 Decode url

--- digitalai/release/integration/wrapper.py
+++ digitalai/release/integration/wrapper.py
@@ -141,9 +141,7 @@ def update_output_context_file(output_context: OutputContext):
             k8s.get_client().replace_namespaced_secret(name, namespace, secret)
         if callback_url:
             logger.debug("Pushing result using HTTP to %s", callback_url)
-            byte_array = base64.b64decode(callback_url)
-            decoded_url = byte_array[16:-16].decode("UTF-8")
-            url = urllib3.util.parse_url(decoded_url)
+            url = base64.b64decode(callback_url).decode("UTF-8")
             logger.debug("Pushing to %s", url)
             urllib3.PoolManager().request("POST", url, headers={'Content-Type': 'application/json'},
                                           body=encrypted_json)
