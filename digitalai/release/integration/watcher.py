6f2f020922c0f553bce543e5deeeb91addc73869
Marin Milina <marin.milina@ag04.com>
2023-07-21 11:20:14 +0200

    S-93185 Refactor watcher

--- digitalai/release/integration/watcher.py
+++ digitalai/release/integration/watcher.py
@@ -31,19 +31,22 @@ def start_input_secret_watcher(on_input_context_update_func, stop):
     field_selector = "metadata.name=" + os.getenv("INPUT_CONTEXT_SECRET")
     namespace = os.getenv("RUNNER_NAMESPACE")
 
+    old_input = None
     w = watch.Watch()
     for event in w.stream(kubernetes_client.list_namespaced_secret, namespace, field_selector=field_selector):
         logger.debug("event is %s", event)
         secret = event['object']
-        old_input = event['raw_object']['data'].get("input")
         logger.debug("old input %s", old_input)
         new_input = secret.data.get("input")
         logger.debug("new input %s", new_input)
 
         # Checking if 'input' field has changed
-        if old_input != new_input:
+        if old_input and old_input != new_input:
             logger.info("Detected input context value change")
             on_input_context_update_func()
+
+        # Set old input value
+        old_input = new_input
 
         # Check if the watcher should be stopped
         if stop.is_set():
