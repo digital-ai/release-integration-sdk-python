c381f139efcf5751fc382ae6d7a5e2a66bfda359
Balaji Venkatesan <83265738+balaji-digital-ai@users.noreply.github.com>
2025-10-07 13:56:50 +0530

    S-125311 Updated for lookup
    * S-125311 Updated for lookup
    
    * S-125311 Updated for warnings

--- digitalai/release/integration/input_context.py
+++ digitalai/release/integration/input_context.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any, List, Dict, Optional
 from dataclasses_json import dataclass_json, LetterCase
 
@@ -62,9 +62,9 @@ class CiDefinition:
     - type (str): Type of the CI.
     - properties (List[PropertyDefinition]): List of properties for the CI.
     """
-    id: str
-    type: str
-    properties: List[PropertyDefinition]
+    id: Optional[str] = None
+    type: Optional[str] = None
+    properties: List[PropertyDefinition] = field(default_factory=list)
 
 
 @dataclass_json
@@ -129,8 +129,8 @@ class AutomatedTaskAsUserContext:
     - username (str): The username to run the task as.
     - password (str): The password for the user.
     """
-    username: Optional[str]
-    password: Optional[str]
+    username: Optional[str] = None
+    password: Optional[str] = None
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
@@ -144,8 +144,10 @@ class ReleaseContext:
     - automated_task_as_user (AutomatedTaskAsUserContext): Context for running
         an automated task as a specific user.
     """
-    id: str
-    automated_task_as_user: AutomatedTaskAsUserContext
+    id: Optional[str] = None
+    automated_task_as_user: Optional[AutomatedTaskAsUserContext] = field(
+        default_factory=AutomatedTaskAsUserContext
+    )
 
 
 @dataclass_json()
@@ -158,6 +160,6 @@ class InputContext:
     - release (ReleaseContext): Context of the release.
     - task (TaskContext): Context of the task.
     """
-    release: ReleaseContext
-    task: TaskContext
+    release: Optional[ReleaseContext] = None
+    task: Optional[TaskContext] = None
 
